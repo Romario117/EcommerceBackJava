75aa6f071619f83474de1de22d7d306358087f9f
Romario117 <rfls_93@hotmail.com>
2023-03-25 17:33:15 -0600

    Se agrega el service de Customer

---
+++ src/main/java/com/romario/ECommerce/services/CustomerService.java
@@ -0,0 +1,10 @@
+package com.romario.ECommerce.services;
+
+import com.romario.ECommerce.entities.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+}
